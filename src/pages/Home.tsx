3b8e950e4a5bb74bd25f77110a5b577f73fbeae0
pasindumadushan.org <pasindumadushan.org@gmail.com>
2024-09-29 09:12:35 +0000

    bg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,14 @@ import Header from "../components/Header";
 function Home() {
   return (
     <div>
-        
+
       <div>
       <Header />
       </div>
 
       <div className="pt-20">
       <Banner/>
+
       </div>
 
     </div>
